1dddea2856fd1ebe539d9eed5e619b9730ba18a0
Mohsen <mohsenmahoski@gmail.com>
2024-09-11 13:09:53 +0330

    chore: resolve build erros

--- components/pages/repository/index.tsx
+++ components/pages/repository/index.tsx
@@ -6,12 +6,13 @@ import React from "react";
 import RepoInfo from "@components/organisms/repoInfo";
 import RepoTypesMobileView from "@components/molecules/repoTypesMobileView";
 import VersionList from "../version";
+import { bulkItemsAtom } from "@atom/bulk";
 import { useRecoilValue } from "recoil";
 import { versionListAtom } from "@atom/version";
 
 const RepoPage = () => {
   const getShowVersionList = useRecoilValue(versionListAtom);
-
+  const getBulkItems = useRecoilValue(bulkItemsAtom);
   return (
     <div className="flex flex-col gap-4 xs:gap-6">
       {getShowVersionList ? (
