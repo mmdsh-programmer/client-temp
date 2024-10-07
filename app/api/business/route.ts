f49b196f17a921d25245062012346d719b5ee696
Mohsen <mohsenmahoski@gmail.com>
2024-10-07 13:24:38 +0330

    chore: force dynamic for business route

--- app/api/business/route.ts
+++ app/api/business/route.ts
@@ -5,6 +5,9 @@ import { headers } from "next/headers";
 import { type NextRequest } from "next/server";
 
 
+// forces the route handler to be dynamic
+export const dynamic = "force-dynamic";
+
 export async function GET(request: NextRequest) {
     try {
         const headersList = headers();
