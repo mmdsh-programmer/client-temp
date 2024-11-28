3b5ef0d7ad6a3d97da35ccbe61e71bd60a1f7eaf
mmdsh-programmer <64894405+mmdsh-programmer@users.noreply.github.com>
2024-11-28 14:33:29 +0330

    fix: expired and unexpected error handling in layout

--- app/[domain]/publish/[id]/[name]/layout.tsx
+++ app/[domain]/publish/[id]/[name]/layout.tsx
@@ -10,31 +10,50 @@ interface IProps {
   children: React.ReactNode;
   params: { id: string; name: string };
 }
-const PublishSlugLayout = async ({ children, params: { id } }: IProps) => {
-  const data = await getThemeAction();
-  const repository = await getRepositoryData(
-    Number.parseInt(toEnglishDigit(decodeURIComponent(id)), 10)
-  );
 
-  if (data && "error" in data) {
+const PublishSlugLayout = async ({ children, params: { id } }: IProps) => {
+  try {
+    const [data, repository] = await Promise.all([
+      getThemeAction(),
+      getRepositoryData(
+        Number.parseInt(toEnglishDigit(decodeURIComponent(id)), 10)
+      ),
+    ]);
+
+    if ("error" in data || "error" in repository) {
+      let errorMessage = "خطا در دریافت اطلاعات";
+      
+      if ("error" in data && data.errorList?.[0]) {
+        const [firstError] = data.errorList ?? [];
+        errorMessage = firstError;
+      } else if ("error" in repository && repository.errorList?.[0]) {
+        const [firstError] = repository.errorList ?? [];
+        errorMessage = firstError;
+      }
+
+      return (
+        <div className="w-screen h-screen grid place-content-center">
+          <Error error={{ message: errorMessage }} />
+        </div>
+      );
+    }
+
     return (
-      <div className="w-full h-full flex justify-center items-center">
-        <Error
-          error={{ message: data.errorList?.[0] ?? "خطا در دریافت اطلاعات" }}
-        />
+      <PublishSlugTemplate
+        repoId={repository.id}
+        repoName={repository.name}
+        themeInfo={data as IThemeInfo}
+      >
+        {children}
+      </PublishSlugTemplate>
+    );
+  } catch {
+    return (
+      <div className="w-screen h-screen grid place-content-center">
+        <Error error={{ message: "خطای غیر منتظره رخ داده است" }} />
       </div>
     );
   }
-
-  return (
-    <PublishSlugTemplate
-      repoId={repository.id}
-      repoName={repository.name}
-      themeInfo={data as IThemeInfo}
-    >
-      {children}
-    </PublishSlugTemplate>
-  );
 };
 
 export default PublishSlugLayout;
