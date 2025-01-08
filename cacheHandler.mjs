8839c4cdcc680505456868e60316331d0da0f4ab
Mohsen <mohsenmahoski@gmail.com>
2025-01-08 10:26:32 +0330

    fix: add getRedisClient

--- cacheHandler.mjs
+++ cacheHandler.mjs
@@ -1,3 +1,4 @@
+/* eslint-disable dot-notation */
 /* eslint-disable import/extensions */
 
 import { CacheHandler } from "@neshca/cache-handler";
@@ -7,47 +8,38 @@ import createClusterHandler from "@neshca/cache-handler/experimental-redis-clust
 
 let cluster;
 export const getRedisClient = async () => {
-  return cluster;
-};
-/* from https://caching-tools.github.io/next-shared-cache/redis */
-CacheHandler.onCreation(async () => {
-  // use redis cluster during build could cause issue https://github.com/caching-tools/next-shared-cache/issues/284#issuecomment-1919145094
-  if (PHASE_PRODUCTION_BUILD !== process.env.NEXT_PHASE) {
-    try {
-      // Create a Redis cluster.
-      cluster  = createCluster({
-        rootNodes: [
-          { url: "redis://10.248.34.142:6379" },
-          { url: "redis://10.248.34.142:6380" },
-          { url: "redis://10.248.34.142:6381" },
-          { url: "redis://10.248.34.142:6382" },
-          { url: "redis://10.248.34.142:6383" },
-          { url: "redis://10.248.34.142:6384" },
-        ],
-        defaults: {
-          username: "clasorclient",
-          password: "YYhi16j",
-          socket: {
-            reconnectStrategy: (retries) => {
-              console.log("Retry redis connection", retries);
-              return 1000;
-            },
-          },
-        },
-      });
 
-
-      // Redis won't work without error handling.
-      cluster.on("error", (e) => {
-        console.log("Redis Error");
-        throw e;
-      });
-
-
-    } catch (error) {
-      console.warn("Failed to create Redis cluster:", error);
-    }
+  if(cluster && cluster["isReady"]){
+       return cluster;
   }
+  cluster = createCluster({
+    rootNodes: [
+      { url: "redis://10.248.34.142:6379" },
+      { url: "redis://10.248.34.142:6380" },
+      { url: "redis://10.248.34.142:6381" },
+      { url: "redis://10.248.34.142:6382" },
+      { url: "redis://10.248.34.142:6383" },
+      { url: "redis://10.248.34.142:6384" },
+    ],
+    defaults: {
+      username: "clasorclient",
+      password: "YYhi16j",
+      socket: {
+        reconnectStrategy: (retries) => {
+          cluster["isReady"] = false;
+          console.log("Retry redis connection", retries);
+          return 1000;
+        },
+      },
+    },
+  });
+
+  // Redis won't work without error handling.
+  cluster.on("error", (e) => {
+    console.log("Redis Error");
+    throw e;
+  });
+
 
   if (cluster) {
     try {
@@ -57,11 +49,11 @@ CacheHandler.onCreation(async () => {
       // Caveat: This will block the server from starting until the cluster is connected.
       // And there is no timeout. Make your own timeout if needed.
       await cluster.connect();
-      cluster.isReady = true;
+      cluster["isReady"] = true;
       console.info("Redis cluster connected.");
     } catch (error) {
       console.warn("Failed to connect Redis cluster:", error);
-      cluster.isReady = false;
+      cluster["isReady"] = false;
 
       console.warn("Disconnecting the Redis cluster...");
       // Try to disconnect the cluster to stop it from reconnecting.
@@ -75,6 +67,20 @@ CacheHandler.onCreation(async () => {
             "Failed to quit the Redis cluster after failing to connect.",
           );
         });
+    }
+  }
+
+  return cluster;
+};
+/* from https://caching-tools.github.io/next-shared-cache/redis */
+CacheHandler.onCreation(async () => {
+  // use redis cluster during build could cause issue https://github.com/caching-tools/next-shared-cache/issues/284#issuecomment-1919145094
+  if (PHASE_PRODUCTION_BUILD !== process.env.NEXT_PHASE) {
+    try {
+      // Create a Redis cluster.
+      cluster  = await getRedisClient();
+    } catch (error) {
+      console.warn("Failed to create Redis cluster:", error);
     }
   }
 
