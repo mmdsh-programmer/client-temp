32e276a101ced50680378a075f95732fb54be63f
z.esmaeilpour <z.esmaeilpour49@gmail.com>
2025-03-16 02:21:48 +0330

    feat: Add setDocumentDomainTags action

---
+++ hooks/domainTags/index.ts
@@ -0,0 +1,15 @@
+import useCreateDomainTag from "./useCreateDomainTag";
+import useGetDomainTags from "./useGetDomainTags";
+import useGetDomainTagById from "./useGetDomainTagById";
+import useEditDomainTag from "./useEditDomainTag";
+import useDeleteDomainTag from "./useDeleteDomainTag";
+import useSetDocumentDomainTags from "./useSetDocumentDomainTags";
+
+export {
+  useCreateDomainTag,
+  useGetDomainTags,
+  useGetDomainTagById,
+  useEditDomainTag,
+  useDeleteDomainTag,
+  useSetDocumentDomainTags,
+}; 
