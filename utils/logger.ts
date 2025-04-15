d7c5245e3253d020709abebe246e4d50ee94e9f3
Mohsen <mohsenmahoski@gmail.com>
2025-04-15 13:52:35 +0330

    chore: update log format

--- utils/logger.ts
+++ utils/logger.ts
@@ -7,6 +7,19 @@ winston.addColors({
   debug: "cyan"
 });
 
+
+const logFormat = winston.format.combine(
+  winston.format.colorize({
+    all: true,
+  }),
+  winston.format.timestamp(),
+  winston.format.printf((info) => {
+    const { message } = info;
+    // Remove newlines while preserving ANSI codes and spaces
+    return `${message as string}`.replaceAll("\\\\\"", "\\\"");
+  }),
+);
+
 const Logger = winston.createLogger({
   transports: [
     new winston.transports.Console({
