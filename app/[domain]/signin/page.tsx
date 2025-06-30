f465b0199ec53e35e7ea7fe5d6ce6e2ffee6077e
z.esmaeilpour <z.esmaeilpour49@gmail.com>
2025-06-30 17:24:08 +0330

    refactor: simplify SignIn component structure

--- app/[domain]/signin/page.tsx
+++ app/[domain]/signin/page.tsx
@@ -1,26 +1,10 @@
 import React, { Suspense } from "react";
-import { ICustomPostData } from "@interface/app.interface";
 import SignInComponent from "@components/pages/signin";
-import { decodeKey } from "@utils/index";
-import { getCustomPostByDomain } from "@service/clasor";
 
-interface IProps {
-  params: {
-    domain: string;
-  };
-}
-const Signin = async ({ params }: IProps) => {
-  const domainHash = decodeKey(params.domain);
-  const { content } = await getCustomPostByDomain(domainHash);
-  const { projectName, logo, projectDescription } = JSON.parse(content ?? "{}") as ICustomPostData;
-
+const Signin = async () => {
   return (
     <Suspense>
-      <SignInComponent
-        projectName={projectName ?? "_"}
-        logo={logo ?? "_"}
-        projectDescription={projectDescription ?? "_"}
-      />
+      <SignInComponent />
     </Suspense>
   );
 };
