5306895eacc28c9592834f83efde2ae0bcff0984
Mohsen <mohsenmahoski@gmail.com>
2025-06-30 15:00:04 +0330

    fix: add useDebouncedCallback for createTinyLink

--- components/organisms/publishBottomNav/publishTinyLink.tsx
+++ components/organisms/publishBottomNav/publishTinyLink.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button, Spinner } from "@material-tailwind/react";
 import React, { useEffect, useState } from "react";
 
@@ -5,6 +7,7 @@ import { ShareIcon } from "@components/atoms/icons";
 import copy from "copy-to-clipboard";
 import { toast } from "react-toastify";
 import useCreateTinyLink from "@hooks/tinyLink/useCreateTinyLink";
+import { useDebouncedCallback } from "use-debounce";
 
 const PublishTinyLink = () => {
   const createTinyLinkHook = useCreateTinyLink();
@@ -17,34 +20,32 @@ const PublishTinyLink = () => {
     }
   };
 
+  const createTinyLink = useDebouncedCallback(() => {
+    createTinyLinkHook.mutate({
+      url: `${window.location.href}`,
+      callBack: (result) => {
+        setShortLink(`${process.env.NEXT_PUBLIC_TINY_BASE_URL}/${result.hash}`);
+      },
+    });
+  }, 100);
+
   useEffect(() => {
     if (typeof window !== "undefined") {
-      createTinyLinkHook.mutate({
-        url: `${window.location.href}`,
-        callBack: (result) => {
-          setShortLink(
-            `${process.env.NEXT_PUBLIC_TINY_BASE_URL}/${result.hash}`
-          );
-        },
-      });
+      createTinyLink();
     }
   }, []);
 
   if (createTinyLinkHook.isPending) {
     return (
-      <div className="flex items-center w-fit">
+      <div className="flex w-fit items-center">
         <Spinner className="h-5 w-5 text-primary" />
       </div>
     );
   }
 
   return (
-    <Button
-      className="w-fit min-w-fit p-3 border-none"
-      onClick={handleCopyLink}
-      variant="outlined"
-    >
-      <ShareIcon className="stroke-white w-6 h-6 block" />
+    <Button className="w-fit min-w-fit border-none p-3" onClick={handleCopyLink} variant="outlined">
+      <ShareIcon className="block h-6 w-6 stroke-white" />
     </Button>
   );
 };
