e6d9ef0ecfb21047aa633404eec40c18fa0da792
z.esmaeilpour <z.esmaeilpour49@gmail.com>
2025-09-21 13:26:30 +0330

    refactor: handle redirect to dashboard in clasor_RPU type page

--- components/pages/repository/index.tsx
+++ components/pages/repository/index.tsx
@@ -9,8 +9,14 @@ import Editor from "@components/organisms/dialogs/editor";
 import { useVersionStore } from "@store/version";
 import { useBulkStore } from "@store/bulk";
 import { useEditorStore } from "@store/editor";
+import useGetUser from "@hooks/auth/useGetUser";
+import { useRepositoryStore } from "@store/repository";
+import { Button, Typography } from "@material-tailwind/react";
+import { useRouter } from "next/navigation";
 
 const Repository = () => {
+  const router = useRouter();
+  const { repo } = useRepositoryStore();
   const getShowVersionList = useVersionStore((state) => {
     return state.versionModalList;
   });
@@ -19,7 +25,34 @@ const Repository = () => {
   });
   const { editorModal: getEditorModal, setEditorModal } = useEditorStore();
 
-  return (
+  const { data: userInfo } = useGetUser();
+
+  const handleBackToDahsboard = () => {
+    router.push("/admin/dashboard");
+  };
+
+  return userInfo?.repository.id === repo?.id ? (
+    <div className="flex flex-col gap-4 h-full items-center justify-center">
+      <Typography
+        className="title_t1 !text-primary_normal"
+        {...({} as React.ComponentProps<typeof Typography>)}
+      >
+        شما به این مخزن دسترسی ندارید.
+      </Typography>
+      <Button
+        {...({} as React.ComponentProps<typeof Button>)}
+        className="back-to-dashboard bg-primary-normal hover:bg-primary-normal active:bg-primary-normal h-10"
+        onClick={handleBackToDahsboard}
+      >
+        <Typography
+          {...({} as React.ComponentProps<typeof Typography>)}
+          className="text__label__button text-white"
+        >
+          بازگشت به صفحه داشبورد
+        </Typography>
+      </Button>
+    </div>
+  ) : (
     <div className="flex flex-col gap-4 xs:gap-6">
       <RepoInfo />
       <CategoryList />
